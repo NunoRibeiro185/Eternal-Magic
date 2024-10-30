a13ed6adf90a460bb7b63b233ed7b45e50a32827
Nuno Ribeiro <nunomonizribeiro@gmail.com>
2024-10-30 21:30:29 +0000

    small changes and fixes

--- Scripts/Attacks/Properties/wave_movement.gd
+++ Scripts/Attacks/Properties/wave_movement.gd
@@ -12,9 +12,9 @@ func _init(attack_resource: AttackResource, sp: Spell) -> void:
 	ar = attack_resource
 	spell = sp
 	collision = spell.collision
-	size = height * 0.05
 	original_position = spell.spawn_point
 	print("Spell global position: ", original_position)
+	
 	if ar.shape == Utility.Shape.Circle:
 		width = ar.width
 		height = ar.height
@@ -22,8 +22,14 @@ func _init(attack_resource: AttackResource, sp: Spell) -> void:
 	add_collisions()
 	
 	if ar.shape == Utility.Shape.Rectangle:
-		spell.collision.shape.points = Utility.select_shape(Utility.Shape.Rectangle, ar.width , size)
-
+		spell.collision.shape.points = Utility.select_shape(Utility.Shape.Rectangle, ar.width , 0)
+		
+	if ar.shape == Utility.Shape.Cone:
+		width = ar.width
+		height = Utility.PLAYER_WIDTH
+		spell.collision.shape.points = Utility.select_shape(ar.shape, 0 , height)
+		spell.collision2.shape.points = Utility.select_shape(ar.shape, 0 , 0)
+	
 	
 func _physics_process(delta: float) -> void:
 	match ar.shape:
@@ -35,16 +41,28 @@ func _physics_process(delta: float) -> void:
 			
 			if width >= ar.attack_range:
 				spell.spell_free()
-				
-			print("Inner Radius:", spell.collision.shape.radius)
-			print("Outer Radius:", spell.collision2.shape.radius)
 			
 		Utility.Shape.Rectangle:
 			spell.global_position += spell.direction * ar.travel_speed * delta
-			print("Distance to: ", original_position.distance_to(spell.global_position))
-			if original_position.distance_to(spell.global_position) >= ar.attack_range - ar.width/2:
+			if original_position.distance_to(spell.global_position) >= ar.attack_range - Utility.PLAYER_WIDTH/2:
 				spell.spell_free()
-			
+				
+		Utility.Shape.Cone:
+			spell.collision.shape.points = Utility.select_shape(Utility.Shape.Cone, width , height)
+			spell.collision2.shape.points = Utility.select_shape(Utility.Shape.Cone, width , max(height - Utility.PLAYER_WIDTH, 0))
+			width = lerpf(0.0, ar.width, height/ar.attack_range)
+			height += ar.travel_speed * delta
+			if height >= ar.attack_range:
+				spell.spell_free()
+		
+		Utility.Shape.Triangle:
+			spell.collision.shape.points = Utility.select_shape(Utility.Shape.Triangle, width , height)
+			spell.collision2.shape.points = Utility.select_shape(Utility.Shape.Triangle, width , max(height - Utility.PLAYER_WIDTH, 0))
+			width = lerpf(0.0, ar.width, height/ar.attack_range)
+			height += ar.travel_speed * delta
+			if height >= ar.attack_range:
+				spell.spell_free()
+				
 func add_collisions():
 	var collision = CollisionShape2D.new()
 	var collision2 = CollisionShape2D.new()
@@ -61,8 +79,12 @@ func add_collisions():
 		collision_shape.radius = width
 		collision_shape2.radius = height
 		
-	else:
+	if ar.shape == Utility.Shape.Rectangle:
 		collision_shape = ConvexPolygonShape2D.new()
+		
+	if ar.shape == Utility.Shape.Cone || ar.shape == Utility.Shape.Triangle:
+		collision_shape = ConvexPolygonShape2D.new()
+		collision_shape2 = ConvexPolygonShape2D.new()
 	
 	spell.collision.shape = collision_shape
 	spell.collision2.shape = collision_shape2
